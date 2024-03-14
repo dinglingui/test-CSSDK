300542f9f5870c2e3a50227b6064a754d7752ca2
dinglingui <dinglingui@kdanmobile.com>
2024-03-14 16:13:58 +0800

    cc--pdf

--- Package.swift
+++ Package.swift
@@ -4,25 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift package",
+    name: "cc--PDF",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Swift package",
-            targets: ["Swift package"]),
+            name: "cc--PDF",
+            targets: ["cc--PDF"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+ 
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Swift package",
-            dependencies: []),
-        .testTarget(
-            name: "Swift packageTests",
-            dependencies: ["Swift package"]),
-    ]
+          .binaryTarget(
+              name: "cc--PDF",
+              url: "https://customers.pspdfkit.com/pspdfkit-xcframework-13.3.3.zip",
+              checksum: "3bf6a841addeab932929cbc9a7c594a8a7e2b8e1480951d5e391d9a07c9234a4"),
+      ]
 )
